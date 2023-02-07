3123e37e3242030b18d438c0a2f52cf0b1ac606c
azizr <azizr@LAPTOP-5IS4ELH8.home>
2023-02-06 19:07:50 -0500

    Tester1- I have uploaded Test1_15 and Test_15_By_Tester1. I committed in my Tester_branch1.

--- src/Selenium_15_Git_package/Test_15_By_Tester1.java
+++ src/Selenium_15_Git_package/Test_15_By_Tester1.java
@@ -2,4 +2,12 @@ package Selenium_15_Git_package;
 
 public class Test_15_By_Tester1 {
 
+	public static void main(String[] args) {
+		String roseflower= "beautiful";
+	System.out.println(roseflower);
+
+
+	}
+
+
 }
